81cd2bac956d787007bf6dd5a6a6200bea8cb878
Aman Bhatt <amanbhatt@Amans-MBP.attlocal.net>
2024-09-30 19:22:52 -0500

    pdf link solution2

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
         <h3><a href={`${process.env.PUBLIC_URL}/Resume.pdf`} target="_blank" rel="noopener noreferrer">TECHNOLOGY INTERN ↗️</a></h3>
         <p>Kashiv BioSciences LLC.</p>
           <hr />
-          <h3><a href=`/Resume.pdf`} target="_blank" rel="noopener noreferrer">BACKEND SYSTEMS ENGINEERING INTERN ↗️</a></h3>
+          <h3><a href="https://amanbhatt.com/Resume.pdf" target="_blank" rel="noopener noreferrer">BACKEND SYSTEMS ENGINEERING INTERN ↗️</a></h3>
           <p>IAMI Authentication LLC.</p>
           <hr />
           <h3><a href="project1.pdf" target="_blank" rel="noopener noreferrer">BIKE SHARING DATA ANALYSIS MODEL AND THESIS ↗️</a></h3>
@@ -60,7 +60,7 @@ const App = () => {
         </div>
         
         <div className="projects">
-          <h3><a href="https://amanbhatt.com/Resume.pdf" target="_blank" rel="noopener noreferrer">RESUME ↗️</a></h3>
+          <h3><a href={`${process.env.PUBLIC_URL}/Resume.pdf`}target="_blank" rel="noopener noreferrer">RESUME ↗️</a></h3>
           <p>Yes, it will pass though an ATS System!</p>
           <hr />
           <h3><a href="https://github.com/AmxnBhxtt" target="_blank" rel="noopener noreferrer">GITHUB ↗️</a></h3>
